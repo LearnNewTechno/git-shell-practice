cbe8bf7015c529609c9f5d1e14757a81212b4a00
Ravikumar Amireddy <ravi.amireddy@encora.com>
2024-08-31 18:42:55 +0530

    udpating script

--- 19-backup.sh
+++ 19-backup.sh
@@ -32,9 +32,9 @@ then
     echo "$DEST_DIR does not exist...Please check"
 fi
 
-# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime "+$DAYS")
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
