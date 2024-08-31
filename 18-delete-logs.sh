b86af4319ce011688ce79afadd49d2335249c298
Ravikumar Amireddy <ravi.amireddy@encora.com>
2024-08-31 15:42:49 +0530

    udpating script

--- 18-delete-logs.sh
+++ 18-delete-logs.sh
@@ -20,7 +20,7 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-while IFS= read -r file
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
     echo "Deleting file: $file"
 
