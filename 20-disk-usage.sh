cd79a494b35626233134aa053ee01c710c3c4df3
Ravikumar Amireddy <amireddyravi@gmail.com>
2024-11-18 21:46:44 +0530

    sample

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5 #real projects, it is usually 75
+DISK_THRESHOLD=5 
 
 
-while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+while IFS= read -r line 
 do
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
